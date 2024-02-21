f235fec97deaffc36d784820c305c4f9979717a3
Jubayed-A <learnigprograming@gmail.com>
2024-02-21 19:30:05 +0600

    checkout others fragment file updated

--- app/src/main/java/com/example/e_comstarterdevsstream/ui/activity/checkout/fragments/CheckoutConfirmationFragment.kt
+++ app/src/main/java/com/example/e_comstarterdevsstream/ui/activity/checkout/fragments/CheckoutConfirmationFragment.kt
@@ -1,60 +1,40 @@
 package com.example.e_comstarterdevsstream.ui.activity.checkout.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.e_comstarterdevsstream.R
+import com.example.e_comstarterdevsstream.databinding.FragmentCheckoutConfirmationBinding
+import com.example.e_comstarterdevsstream.ui.activity.checkout.activity.CheckoutThankYouActivity
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CheckoutConfirmationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CheckoutConfirmationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var binding: FragmentCheckoutConfirmationBinding
+
+    companion object {
+        // Factory method to create a new instance of the fragment for stepper
+        fun newInstance(): CheckoutConfirmationFragment {
+            return CheckoutConfirmationFragment()
         }
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_checkout_confirmation, container, false)
+    ): View {
+        binding = FragmentCheckoutConfirmationBinding.inflate(inflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CheckoutConfirmationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CheckoutConfirmationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnPlaceOrder.setOnClickListener {
+            startActivity(Intent(requireContext(), CheckoutThankYouActivity::class.java))
+        }
     }
+
 }
--- app/src/main/java/com/example/e_comstarterdevsstream/ui/activity/checkout/fragments/CheckoutInfoFragment.kt
+++ app/src/main/java/com/example/e_comstarterdevsstream/ui/activity/checkout/fragments/CheckoutInfoFragment.kt
@@ -5,46 +5,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.e_comstarterdevsstream.databinding.FragmentCheckoutPaymentsBinding
+import com.example.e_comstarterdevsstream.databinding.FragmentCheckoutInfoBinding
 import com.example.e_comstarterdevsstream.ui.activity.checkout.stepper.ButtonClickListener
 
-class CheckoutPaymentFragment : Fragment() {
+class CheckoutInfoFragment : Fragment() {
 
-    private lateinit var binding: FragmentCheckoutPaymentsBinding
+    private lateinit var binding: FragmentCheckoutInfoBinding
     private var buttonClickListener: ButtonClickListener? = null
 
     companion object {
         // Factory method to create a new instance of the fragment for stepper
-        fun newInstance(): CheckoutPaymentFragment {
-            return CheckoutPaymentFragment()
+        fun newInstance(): CheckoutInfoFragment {
+            return CheckoutInfoFragment()
         }
     }
 
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentCheckoutPaymentsBinding.inflate(inflater)
+        binding = FragmentCheckoutInfoBinding.inflate(inflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.tvNewAddress.setOnClickListener {
+            binding.checkoutInfoSection.visibility = View.GONE
+            binding.checkoutNewAddress.visibility = View.VISIBLE
+        }
+        binding.btnSave.setOnClickListener {
+            binding.checkoutInfoSection.visibility = View.VISIBLE
+            binding.checkoutNewAddress.visibility = View.GONE
+        }
+
         binding.btnContinue.setOnClickListener {
             // Trigger the interface method when the button is clicked
             buttonClickListener?.onButtonClicked()
         }
 
-        binding.tvNewCards.setOnClickListener {
-            binding.checkoutPaymentSection.visibility = View.GONE
-            binding.checkoutPaymentNewCardSection.visibility = View.VISIBLE
-        }
-        binding.btnSave.setOnClickListener {
-            binding.checkoutPaymentSection.visibility = View.VISIBLE
-            binding.checkoutPaymentNewCardSection.visibility = View.GONE
-        }
 
     }
 
@@ -52,5 +53,6 @@ class CheckoutPaymentFragment : Fragment() {
     fun setButtonClickListener(listener: ButtonClickListener) {
         buttonClickListener = listener
     }
+
 
 }
